4a8cf7428373fcab9b6071656021999c14cf1aef
Ntt37Vp <fuckspammers@protonmail.com>
2023-10-07 03:49:13 -0400

    Week 1 "relating" starting notes

---
+++ Week-1-Relating/notes.sql
@@ -0,0 +1,17 @@
+-- Week 1 - Relating
+
+-- the new version of the longlist.db (version week 1) has more tables:
+-- authored, authors, books, publishers, ratings, translated, translators
+
+-- Relationships:
+-- one to one, one to many, many to one, many to many
+-- ER Diagram or "Entity Relationship Diagrams"
+-- uses cross foot notation
+
+
+
+
+
+
+
+-- stop @
