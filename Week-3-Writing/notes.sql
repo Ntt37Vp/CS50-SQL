074627349036b34c15dfffa9ea3d76357269bc26
Ntt37Vp <fuckspammers@protonmail.com>
2023-10-18 00:03:19 +0800

    importing csv

--- Week-3-Writing/notes.sql
+++ Week-3-Writing/notes.sql
@@ -1,6 +1,6 @@
 -- Week 3: Writing
 
--- Inserting
+-- INSERTING
 -- approach 1: specify both column and values
 INSERT INTO table_name (column1, column2, column3, ...)
 VALUES (value1, value2, value3, ...); 
@@ -19,3 +19,39 @@ VALUES (2, 'Farmers Working at Dawn', '11.6152', '1911-08-03');
 -- an example wherein the Primary Key was not placed
 INSERT INTO "collections" ("title", "accession_num", "acquired")
 VALUES ('Spring Outing', '14.76', '1914-01-08');
+
+-- inserting multiple lines at once
+INSERT INTO "collections" ("title", "accession_num", "acquired")
+VALUES
+('Imaginative Landscape', '56.496', NULL),
+('Peonies and butterflies', '06.18', '1906-01-01');
+
+
+
+-- IMPORTING
+-- csv can be used to import into sql
+-- Let's DELETE the initial version of our mfa.db
+-- rm mfa.db
+-- re-create a blank mfa.db
+-- using .IMPORT
+.import --csv --skip 1 mfa.csv collections
+
+-- importing csv without the primary key column
+-- use the newly created mfa_v2.csv
+-- let's use a temporary table, do not skip header
+.import --csv mfa_v2.csv temp
+
+-- inserting rows from separate table 
+INSERT INTO table0 (column0, ...)
+SELECT column0, ... FROM table1;
+
+-- actual code for our example:
+INSERT INTO "collections" ("title", "accession_num", "acquired")
+SELECT *
+FROM temp;
+-- OR verbose:
+INSERT INTO "collections" ("title", "accession_num", "acquired")
+SELECT ("title", "accession_num", "acquired")
+FROM temp;
+-- once the data is transferred to the official table, drop the temp table
+-- DROP TABLE temp;
