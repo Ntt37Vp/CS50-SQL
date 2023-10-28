31f10067306c575f838fbcbe27cbb9bafee8c154
Ntt37Vp <fuckspammers@protonmail.com>
2023-10-28 11:20:14 +0800

    VACUUM, Concurrency, modified gitignore

--- Week-5-Optimizing/notes.sql
+++ Week-5-Optimizing/notes.sql
@@ -73,4 +73,40 @@ ON "people" ("name");
  
  -- PARTIAL INDEX
  -- index that only contains a subset of rows from a table
- 
+ -- let's create a partial index
+ CREATE INDEX "recents"
+ ON "movies" ("title")
+ WHERE "year" = 2023;
+-- then let's query
+SELECT "title" FROM "movies" 
+WHERE "year" = 2023;
+-- Run Time: real 0.124 user 0.009492 sys 0.030444
+EXPLAIN QUERY PLAN 
+--  QUERY PLAN
+--SCAN movies USING INDEX recents
+
+-- VACUUM
+-- to give back the unused bytes
+-- unix cmd: du -b filename
+-- run VACUUM in sqlite then run 'du -b' in order to get the actual memory
+
+
+-- CONCURRENCY
+-- A transaction is an individual unit of work
+-- A.C.I.D.
+-- Atomicity, Consistency, Isolation, Durability
+-- syntax
+BEGIN transaction;
+...
+COMMIT;
+-- Scenario: Say, Alice sends her 10 bal to Bob
+-- if we do this using 2 txn: Alice deduct 10 , then Bob add 10
+-- if we use BEGIN TRANSACTION instead:
+BEGIN TRANSACTION;
+UPDATE "accounts"
+SET "balance" = "balance" + 10
+WHERE "id" = 2;
+UPDATE "accounts"
+SET "balance" = "balance" - 10
+WHERE "id" = 1;
+COMMIT;
