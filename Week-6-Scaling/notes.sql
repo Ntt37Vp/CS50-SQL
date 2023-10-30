2e76953efc812ffcd098f6eb127be194935070ff
Ntt37Vp <fuckspammers@protonmail.com>
2023-10-30 22:38:51 +0800

    mysql

--- Week-6-Scaling/notes.sql
+++ Week-6-Scaling/notes.sql
@@ -2,6 +2,20 @@
 
 -- MySQL
 -- mysql -u username -h 127.0.9.1 -P 3306 -p
+CREATE DATABASE `mbta`;
+SHOW DATABASES;
+USE `mbta`;
+
+-- DataTypes in MySQL
+-- tinyint (1byte), smallint, mediumint, int, bigint (8byte)
+
+CREATE TABLE `cards` (
+    `id` INT AUTO_INCREMENT,
+    PRIMARY KEY(`id`)
+);
+SHOW TABLES;
+DESCRIBE `cards`;
+
 
 -- Stored Procedures
 
